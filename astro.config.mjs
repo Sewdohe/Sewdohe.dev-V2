41bb7f474b4af050979a5809d393d8c7c60ba889
Sewdohe <sewdohe@protonmail.com>
2026-07-24 21:02:32 -0400

    added support for tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,62 +8,70 @@ import icon from 'astro-icon';
 
 import expressiveCode from 'astro-expressive-code';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sewdohe.dev',
-    integrations: [expressiveCode({
-        // You can set configuration options here
-        themes: ['kanagawa-wave'],
-        useDarkModeMediaQuery: false,
-        themeCssSelector: () => ':root',
-        styleOverrides: {
-            codeFontFamily: '"Operator Mono", monospace',
-        },
-    }), mdx(), sitemap(), icon()],
-    fonts: [
-        {
-            provider: fontProviders.local(),
-            name: 'Atkinson',
-            cssVariable: '--font-atkinson',
-            fallbacks: ['sans-serif'],
-            options: {
-                variants: [
-                    {
-                        src: ['./src/assets/fonts/atkinson-regular.woff'],
-                        weight: 400,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                    {
-                        src: ['./src/assets/fonts/atkinson-bold.woff'],
-                        weight: 700,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                ],
-            },
-        },
-        {
-            provider: fontProviders.local(),
-            name: 'Operator Mono',
-            cssVariable: '--font-operator',
-            fallbacks: ['sans-serif'],
-            options: {
-                variants: [
-                    {
-                        src: ['./src/assets/fonts/OperatorMono-Book.otf'],
-                        weight: 400,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                    {
-                        src: ['./src/assets/fonts/OperatorMono-Bold.otf'],
-                        weight: 700,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                ],
-            },
-        },
-    ],
+  site: 'https://sewdohe.dev',
+
+  integrations: [expressiveCode({
+      // You can set configuration options here
+      themes: ['kanagawa-wave'],
+      useDarkModeMediaQuery: false,
+      themeCssSelector: () => ':root',
+      styleOverrides: {
+          codeFontFamily: '"Operator Mono", monospace',
+      },
+  }), mdx(), sitemap(), icon()],
+
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+      {
+          provider: fontProviders.local(),
+          name: 'Operator Mono',
+          cssVariable: '--font-operator',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/OperatorMono-Book.otf'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/OperatorMono-Bold.otf'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
